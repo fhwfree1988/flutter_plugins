7df63d15fb7dec90e3fb9daf3e505240217b07ea
Foad <FuadHr>
2022-06-14 10:21:27 +0430

    first commit

---
+++ tab/lib/sample/tab_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    _tabController = new TabController(length: 4, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              child: Center(
+                child: Text(
+                  'Your Widget if you want',
+                ),
+              ),
+              color: Colors.blue,
+            ),
+            TabBar(
+              unselectedLabelColor: Colors.black,
+              labelColor: Colors.red,
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.person),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.add,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.deck,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.child_care,
+                  ),
+                ),
+              ],
+              controller: _tabController,
+              indicatorSize: TabBarIndicatorSize.tab,
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(
+                    child: Text(
+                      'Screen 1',
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Screen 2',
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Screen 3',
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Screen 4',
+                    ),
+                  ),
+                ],
+                controller: _tabController,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
